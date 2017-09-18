4393b834bba3fd93ac847f94981c6ca706dc9f1d
PoC-Yeh <allisonxp7@gmail.com>
2017-09-18 14:29:03 +0800

    Update get_info_and_posts_implement.py

--- get_info_and_posts_implement.py
+++ get_info_and_posts_implement.py
@@ -6,16 +6,16 @@ import progressbar
 from get_info_and_posts import *
 
 
-fanpage_XXX_comment = get_facebookFanPage_comment("your_token", "fanpage_name")
-show_posts(fanpage_XXX_comment[0])
-
-
 fanpage_XXX_info = fanpage_info("your_token", "fanpage_name")
 show_fanpage_info(fanpage_XXX_info)
 
 
-with open("fanpage_XXX_comment_pickle.txt", "wb")as c:
-    pickle.dump(fanpage_XXX_comment, c)
-  
-with open("fanpage_XXX_info_pickle.txt", "wb")as c:
-    pickle.dump(fanpage_XXX_info, c)
+fanpage_XXX_comment = get_facebookFanPage_comment("your_token", "fanpage_name")
+show_posts(fanpage_XXX_comment[0])
+
+
+combine = [fanpage_XXX_info, fanpage_XXX_comment]
+
+
+with open("fanpage_XXX_pickle.txt", "wb")as c:
+    pickle.dump(combine, c)
